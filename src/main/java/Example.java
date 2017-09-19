9832cd16b3d4b410f8186837a5eb1ef5e518c9d3
vthiagarajan <vthiagarajan@homeaway.com>
2017-09-19 11:29:01 -0500

    Update Twilio Jar to the latest version

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -2,42 +2,43 @@
  * Created by vthiagarajan on 9/6/17.
  */
 // Install the Java helper library from twilio.com/docs/java/install
-    import java.net.URI;
-    import java.net.URISyntaxException;
-    import com.twilio.sdk.Twilio;
-    import com.twilio.sdk.client.TwilioRestClient;
-    import com.twilio.sdk.resource.api.v2010.account.Call;
-    import com.twilio.sdk.type.PhoneNumber;
-    import com.twilio.twiml.Play;
-    import com.twilio.twiml.TwiMLException;
-    import com.twilio.twiml.VoiceResponse;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Call;
+import com.twilio.rest.api.v2010.account.CallCreator;
+import com.twilio.type.PhoneNumber;
 
 public class Example {
     // Find your Account Sid and Token at twilio.com/user/account
     public static final String ACCOUNT_SID = "ACe9bd589acf25766773e0eec2f1f99cfb";
     public static final String AUTH_TOKEN = "ef3a04b85d073b19c6dff0cf478a5d63";
 
-    public static void main(String[] args) throws URISyntaxException {
-        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+    public static void main(final String[] args) {
+        Twilio.init("ACe9bd589acf25766773e0eec2f1f99cfb", "ef3a04b85d073b19c6dff0cf478a5d63");
 
-        // Find your Account Sid and Auth Token at twilio.com/console
-        // public static final String ACCOUNT_SID = "AC123";
-        //public static final String AUTH_TOKEN = "456bef";
+        try {
 
-        TwilioRestClient client = new TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN);
+            CallCreator creator =
+                Call.creator(
+                    ACCOUNT_SID,
+                    new PhoneNumber("+16824018808"),
+                    new PhoneNumber("(682) 200-8898"),
+                    new URI("http://vinodh.adaptainer.io/services/playmessage?recordFilePath=https://storage.googleapis.com/jonsnow/dragon.mp3")
+                ).setMachineDetection("DetectMessageEnd")
+                .setMachineDetectionTimeout(120);
 
-        PhoneNumber to = new PhoneNumber("+16824018808"); // Replace with your phone number
-        PhoneNumber from = new PhoneNumber("(682) 200-8898"); // Replace with a Twilio number
-        URI uri = URI.create("http://vinodh.adaptainer.io/playmessage?recordFilePath=https://storage.googleapis.com/jonsnow/dragon.mp3");
+            Call call = creator.create();
 
+            System.out.println(call.getSid());
+            System.out.println(call.getStatus().toString());
 
-        // Make the call
-        Call call = Call.create(ACCOUNT_SID, to, from,
-            uri).execute(client);
-        // Call call = Call.creator(to, from, uri).create(client);
-        // Print the call SID (a 32 digit hex like CA123..)
-        System.out.println(call.getSid());
-
+        } catch (URISyntaxException e) {
+            System.err.println("womp womp");
+            System.exit(1);
+        }
     }
 
 }
